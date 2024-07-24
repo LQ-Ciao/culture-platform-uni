f3fe28282dc905f7bbd93f0177fa887512b7526f
rikkun <963484147@qq.com>
2024-07-24 10:16:07 +0800

    [mod]收藏/点赞功能按钮

---
+++ pages/userMsg/countBtn.vue
@@ -0,0 +1,71 @@
+<template>
+  <view class="view-more-component">
+    <image class="background-image" :src="backgroundImage" mode="widthFix"></image>
+    <view class="text-container">
+      <uni-icons color="#ffffff" :type="iconType" size="30"></uni-icons>
+      <text class="view-more-text">{{ text }}</text>
+      <text class="view-more-count">{{ count }}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundImage: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    count: {
+      type: Number,
+      default: 0
+    },
+    iconType:{
+      type: String,
+      default: 'contact'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.view-more-component {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 30px auto;
+}
+
+.background-image {
+  width: 100%;
+  z-index: 0;
+}
+
+.text-container {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 10px;
+  z-index: 1;
+}
+
+.view-more-text {
+  color: white;
+  font-size: 18px;
+  margin-left: 10px;
+}
+
+.view-more-count {
+  color: white;
+  font-size: 18px;
+  margin-left: auto;
+  padding-right: 30px;
+}
+</style>
--- pages/userMsg/userMsg.vue
+++ pages/userMsg/userMsg.vue
@@ -1,144 +1,87 @@
 <template>
   <view>
-    <view class="center" style="height: 250px;" :style="{background: 'url('+backgroundUrl+')'}">
-      <uni-row style="height: 100px">
-        <uni-col :span="9">
-          <image src="/static/icon/userMsg/default_avatar.png" style="width: 100px" mode="widthFix"/>
+    <view class="header">
+      <image src="https://s2.loli.net/2024/07/23/RfKvVOHwuIBPS8i.png"
+             mode="widthFix"
+             style="width: 100%;z-index: 0;"
+      >
+      </image>
+      <view class="base-msg">
+        <image :src="defaultAvatar" mode="widthFix" class="avatar"></image>
+        <text class="username">{{ username }}</text>
+        <button class="mini-btn"
+                type="default" size="mini"
+                style="margin-left: 100px;background-color: #d9fffe">编辑资料
+        </button>
+      </view>
+    </view>
+
+    <view style="background-color: #fffbf5;height: 200px;margin: 20px 20px 20px 20px">
+      <uni-row gutter="50">
+        <uni-col :span="12">
+          <count-btn
+              background-image="https://s2.loli.net/2024/07/23/7rHxT9bvEDa3YUF.png"
+              text="收藏"
+              :count="thumbCount"
+              icon-type="hand-up-filled"
+          ></count-btn>
+        </uni-col>
+        <uni-col :span="12">
+          <count-btn
+              background-image="https://s2.loli.net/2024/07/23/nzDAdpgsjPIbSfq.png"
+              text="点赞"
+              :count="collectCount"
+              icon-type="star-filled"
+          ></count-btn>
         </uni-col>
 
-        <uni-col :span="15">
-          <uni-row style="height: 100px">
-            <uni-col>
-              <text class="phoneNumber">
-                {{ phoneNumber }}
-              </text>
-            </uni-col>
-            <uni-col style="margin-top: 20px">
-              <button plain="true" size="mini" style="border-radius: 25px;color: #ffffff">编辑资料</button>
-            </uni-col>
-          </uni-row>
-        </uni-col>
       </uni-row>
-    </view>
-
-    <view>
-      <uni-card>
-        <uni-row>
-          <uni-col :span="12" class="centerText">
-            <view class="innerText">
-              <text style="font-size: 50px">{{ collectCount }}</text>
-              <br/>
-              <text>收藏</text>
-            </view>
-          </uni-col>
-          <uni-col :span="12" class="centerText">
-            <view class="innerText">
-              <text style="font-size: 50px">{{ thumbCount }}</text>
-              <br/>
-              <text>点赞</text>
-            </view>
-          </uni-col>
-        </uni-row>
-      </uni-card>
-    </view>
-
-    <view>
-      <uni-card style="padding: 24px 0 24px 0">
-        <view v-for="(item,index) in this.menuItems" style="margin: 24px 0 24px 0">
-          <uni-row>
-            <uni-col :span="3">
-              <image :src="item.icon" style="width: 30px" mode="widthFix"/>
-            </uni-col>
-            <uni-col :span="20">
-              <text class="menuText"> {{ item.label }}</text>
-            </uni-col>
-            <uni-col :span="1">
-              <uni-icons type="right" size="30"></uni-icons>
-            </uni-col>
-          </uni-row>
-          <view v-if="index< menuItems.length-1" class="divLine"></view>
-        </view>
-      </uni-card>
-    </view>
-
-    <view style="margin: 0 24px 0 24px">
-      <button type="warn" size="large" class="logout-button">退出登录</button>
     </view>
   </view>
 </template>
 
 <script>
+import countBtn from "./countBtn.vue";
+
 export default {
+  components: {countBtn},
   data() {
     return {
-      collectCount: 0,
+      defaultAvatar: 'https://s2.loli.net/2024/07/23/BqhJYG9RAFc54g7.png',
+      username: "1111",
       thumbCount: 0,
-      phoneNumber: '13111111111',
-      backgroundUrl: '/static/usrMsg_background.png',
-      menuItems: [{
-        label: "账号信息",
-        icon: "/static/icon/userMsg/account.png",
-        url: "",
-      }, {
-        label: "我的收藏",
-        icon: "/static/icon/userMsg/collection.png",
-        url: "",
-      }, {
-        label: "我的咨询",
-        icon: "/static/icon/userMsg/consulting.png",
-        url: "",
-      }, {
-        label: "我的评论",
-        icon: "/static/icon/userMsg/comment.png",
-        url: "",
-      }]
-    };
-  },
-  methods: {},
-};
+      collectCount: 0,
+    }
+  }
+}
 </script>
 
-<style scoped>
-.center {
-  flex: auto;
+<style>
+.header {
+  position: relative;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 }
 
-.phoneNumber {
-  font-size: 20px;
-  color: #fff;
-}
-
-.centerText {
+.base-msg {
   display: flex;
-  justify-content: center;
-}
-
-.innerText {
-  text-align: center;
-  margin-top: 20px;
-}
-
-.menuText {
-  font-size: 20px;
-  color: #333333;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  position: absolute;
+  color: white;
+  font-size: 18px;
+  z-index: 1;
 }
 
-.logout-button {
-  width: 100%;
-  border-radius: 40px;
+.avatar {
+  width: 100px;
+  height: 100px;
 }
 
-/*分割线样式*/
-.divLine {
-  background: #E0E3DA;
-  width: 100%;
-  height: 1rpx;
+.username {
+  margin-left: 20px
 }
+
+
 </style>

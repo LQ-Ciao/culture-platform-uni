f38b2ac8a3a3267ebd92d6ab5f246eab9d16ce02
rikkun <963484147@qq.com>
2024-08-29 15:45:45 +0800

    [mod]个人信息页面菜单文字与图标对齐

--- pages/userMsg/menuItem.vue
+++ pages/userMsg/menuItem.vue
@@ -1,16 +1,16 @@
 <template>
   <view style="margin: 20px 0 20px">
-  <uni-row class="menu-item">
-    <uni-col :span="1" :offset="1">
-      <text :class="iconType" style="font-size: 30px"></text>
-    </uni-col>
-    <uni-col :span="18" :offset="1">
-      <text>{{ menuText }}</text>
-    </uni-col>
-    <uni-col :span="1">
-      <uni-icons type="forward" size="18"></uni-icons>
-    </uni-col>
-  </uni-row>
+    <uni-row class="menu-item">
+      <uni-col :span="1" :offset="1" class="col-item">
+        <text :class="iconType" style="font-size:20px"></text>
+      </uni-col>
+      <uni-col :span="18" :offset="1" class="col-item">
+        <text>{{ menuText }}</text>
+      </uni-col>
+      <uni-col :span="1" class="col-item">
+        <uni-icons type="forward" size="18"></uni-icons>
+      </uni-col>
+    </uni-row>
   </view>
 </template>
 
@@ -33,8 +33,14 @@ export default {
 .menu-item {
   display: flex;
   flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin-top: 5px;
+  align-items: center; /* 竖直方向居中 */
+  justify-content: flex-start; /* 水平方向左对齐 */
+  margin-top: 10px;
+}
+
+.col-item {
+  display: flex;
+  align-items: center; /* 竖直方向居中 */
+  justify-content: flex-start; /* 水平方向左对齐 */
 }
 </style>
